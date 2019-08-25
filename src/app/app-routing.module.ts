808612bd826239dadde6c89dd30c9d509d90da69
Kiran Jose <kiranjose22@gmail.com>
2019-08-25 23:37:24 +0530

    added main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignupUserComponent } from './signup-user/signup-user.component';
+import { SignupMentorComponent } from './signup-mentor/signup-mentor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'index', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup-user', component: SignupUserComponent},
+  {path: 'signup-mentor', component: SignupMentorComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, LoginComponent,PageNotFoundComponent,SignupMentorComponent,SignupUserComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignupUserComponent } from './signup-user/signup-user.component';
+import { SignupMentorComponent } from './signup-mentor/signup-mentor.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    FooterComponent
+    NavbarComponent,   
+    FooterComponent,
+    routingComponents,
+    PageNotFoundComponent,
+    SignupUserComponent,
+    SignupMentorComponent
   ],
   imports: [
     BrowserModule,
