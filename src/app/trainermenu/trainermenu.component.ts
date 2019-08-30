9a273704d5d2cb9e1f6a3045836c6ddb5489cd34
Kiran Jose <kiranjose22@gmail.com>
2019-08-30 12:29:12 +0530

    update

--- src/app/trainermenu/trainermenu.component.ts
+++ src/app/trainermenu/trainermenu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginprocessService } from '../loginprocess.service';
 import { DataReaderService } from '../data-reader.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-trainermenu',
@@ -14,6 +15,7 @@ export class TrainermenuComponent implements OnInit {
   currentUserData
   completedUserData
   loggedin
+  registrationForm
 
   constructor(
     private loginProcess: LoginprocessService,
@@ -31,16 +33,39 @@ export class TrainermenuComponent implements OnInit {
       this.currentUserData = data
     });
 
+    this.registrationForm = new FormGroup({
+      userName: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      password: new FormControl(''),
+      // newpassword: new FormControl(''),
+      experience: new FormControl(''),
+      timezone: new FormControl('undefined'),
+      timeslot: new FormControl('undefined'),
+
+      linkedin: new FormControl(''),
+      skills: new FormControl(''),
+      videos: new FormControl(''),
+      blogs: new FormControl(''),
+      ppts: new FormControl(''),
+      demos: new FormControl(''),
+
+      role: new FormControl('mentor')
+    });
+
+
 
 
   }
 
   ngOnInit() {
 
+
     this.currentUser = this.loginProcess.getCurrentUser();
     this.loggedin = this.loginProcess.loggedin;
     if (!this.loggedin)
       this.route.navigate(['/home']);
+    this.registrationForm.patchValue(this.currentUser);
 
   }
 
